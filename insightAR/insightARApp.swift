448864d427743808ef700c1dd8bcf1f3c19871f3
aryanc3927 <96081672+aryanc3927@users.noreply.github.com>
2024-05-13 13:56:53 -0700

    Initial Commit

---
+++ insightAR/insightARApp.swift
@@ -0,0 +1,17 @@
+//
+//  insightARApp.swift
+//  insightAR
+//
+//  Created by Aryan Chaudhary on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct insightARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
